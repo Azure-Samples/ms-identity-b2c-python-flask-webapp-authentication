c50337927e1d44a28112246fda152be368b4d54b
idg-sam <66391663+idg-sam@users.noreply.github.com>
2020-10-23 06:00:00 -0700

    latest dependencies and updates

--- app_config.py
+++ app_config.py
@@ -1,6 +1,10 @@
 import os
 ### YOUR APP CONFIGS ###
-# this is required for encrypting flask session cookies:
-SECRET_KEY = os.environ.get('SAMPLE_APP_ENCRYPTION_KEY','enter-a-great-key') # should be key vault or other secure location.
 # write to filesystem:
 SESSION_TYPE = 'filesystem'
+
+
+
+
+# sample title in navbar
+SAMPLE_DESCRIPTION = 'B2C Authentication: Use MSAL Python to sign users into a B2C tenant'
