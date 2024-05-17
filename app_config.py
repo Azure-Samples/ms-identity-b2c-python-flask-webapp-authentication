f303c0c8f16a7429d3925c97124f875802f1880b
Brian Melton-Grace <brianmel@microsoft.com>
2024-05-17 08:38:05 -0700

    Archiving repo

--- app_config.py
+++
@@ -1,9 +0,0 @@
-### YOUR APP CONFIGS ###
-# write to filesystem:
-SESSION_TYPE = 'filesystem'
-
-
-
-
-# sample title in navbar
-SAMPLE_DESCRIPTION = 'B2C Authentication: Use MSAL Python to sign users into your Azure AD B2C tenant'
